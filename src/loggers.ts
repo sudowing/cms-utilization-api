50acd1baf8f74ec13d73f68c676437110e3c51f8
Joe Wingard <open-source@joewingard.com>
2019-04-11 22:04:17 -0400

    setup linting

--- src/loggers.ts
+++ src/loggers.ts
@@ -1,9 +1,9 @@
 import * as winston from "winston";
 
 export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
   defaultMeta: { service: "user-service" },
+  format: winston.format.json(),
+  level: "info",
   transports: [
     //
     // - Write to all logs with level `info` and below to `combined.log`
