697f5d0ccd9bed4ddf382b889cec383fe9dbcd39
Joe Wingard <open-source@joewingard.com>
2019-04-17 19:35:39 -0400

    added ts support to npi-registry module

--- src/modules/npi-registry/npi-registry.services.ts
+++ src/modules/npi-registry/npi-registry.services.ts
@@ -1,12 +1,13 @@
 import { npiRegistry } from "../../network-sources";
+import { RegistryProfile } from "./npi-registry.interfaces";
 
-export const getProvider = async (npi: number) => {
+export const getProvider = async (npi: number): Promise<RegistryProfile|null> => {
     const axiosResponse = await npiRegistry.get("", {
         params: {
             number: npi,
         },
     });
     const { status, data } = axiosResponse;
-    const output = (status === 200 && data.result_count === 1) ? data.results[0] : null;
+    const output = (status === 200 && data.result_count === 1) ? data.results[0] as RegistryProfile : null;
     return output;
 }
