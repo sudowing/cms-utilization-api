50bc4542504fd9620076d47d500c4fc2934b466f
Joe Wingard <open-source@joewingard.com>
2019-04-19 23:09:03 -0400

    remove logging

--- src/modules/cms-db/cms-db.resolvers.ts
+++ src/modules/cms-db/cms-db.resolvers.ts
@@ -3,50 +3,32 @@ import * as svc from "./cms-db.services";
 export const resolvers = {
   Query: {
     async provider(obj: any, args: any, context: any, info: any) {
-      const result = await svc.provider(1003082041);
-      console.log('provider(1003082041);', result);
-      return result;
+      return await svc.provider(1003082041);
     },
     async providerIndividual(obj: any, args: any, context: any, info: any) {
-      const result = await svc.providerIndividual(1003082041);
-      console.log('providerIndividual(1003082041);', result);
-      return result;
+      return await svc.providerIndividual(1003082041);
     },
     async providerOrganization(obj: any, args: any, context: any, info: any) {
-      const result = await svc.providerOrganization(1003082041);
-      console.log('providerOrganization(1003082041);', result);
-      return result;
+      return await svc.providerOrganization(1003082041);
     },
     async providerPerformances(obj: any, args: any, context: any, info: any) {
-      const result = await svc.providerPerformances("", 1003082041);
-      console.log('providerPerformances("", 1003082041);', result);
-      return result;
+      return await svc.providerPerformances("", 1003082041);
     },
 
     async service(obj: any, args: any, context: any, info: any) {
-      const result = await svc.service("00120");
-      console.log('service("00120");', result);
-      return result;
+      return await svc.service("00120");
     },
     async servicePerformance(obj: any, args: any, context: any, info: any) {
-      const result = await svc.servicePerformance("00120");
-      console.log('servicePerformance("00120");', result);
-      return result;
+      return await svc.servicePerformance("00120");
     },
     async serviceProviderPerformance(obj: any, args: any, context: any, info: any) {
-      const result = await svc.serviceProviderPerformance("00120");
-      console.log('serviceProviderPerformance("00120");', result);
-      return result;
+      return await svc.serviceProviderPerformance("00120");
     },
     async serviceProviderPerformanceSummary(obj: any, args: any, context: any, info: any) {
-      const result = await svc.serviceProviderPerformanceSummary(1003082041);
-      console.log('serviceProviderPerformanceSummary(1003082041);', result);
-      return result;
+      return await svc.serviceProviderPerformanceSummary(1003082041);
     },
     async serviceProviderPerformanceSummaryType(obj: any, args: any, context: any, info: any) {
-      const result = await svc.serviceProviderPerformanceSummaryType(0);
-      console.log('serviceProviderPerformanceSummaryType(1);', result);
-      return result;
+      return await svc.serviceProviderPerformanceSummaryType(0);
     },
 
   },
